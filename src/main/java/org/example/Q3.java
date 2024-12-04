11c8fb1c30fa6d19115cfb28c53644b930e0eafa
Oleksandr <116847393+iAmOl3ks4ndr@users.noreply.github.com>
2024-12-04 17:06:29 +0000

    Task 3 Update (read commit description)
    ArrayList was used instead of stack in the previous version of Q3.java file

--- src/main/java/org/example/Q3.java
+++ src/main/java/org/example/Q3.java
@@ -1,22 +1,20 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+// Importing libraries
+import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Q3 {
     public static boolean validate(String filename) throws FileNotFoundException {
-        // Declaring variables and file reader that will go through the file
+        // Declaring variables
         boolean valid = true;
-        String code = "";
-        ArrayList<String> openingTags = new ArrayList<>();
-        Scanner fileReader = new Scanner(new File(filename));
+        String code;
+        Deque<String> stack = new ArrayDeque<>();
 
         // If the file is empty - everything is correct with the code and there is no need to check for validation
         try {
-            code = fileReader.nextLine();
+            code = new Scanner(new File(filename)).nextLine();
         }
         catch (NoSuchElementException e) {
             return true;
@@ -28,18 +26,15 @@ public class Q3 {
         // Going through array of all tags ignoring all <br> tags
         for(String tag : tags) {
             // If tag is opening tag - adding it to ArrayList with opening tags
-            if(!tag.equals("<br>") && tag.charAt(1) != '/') {
-                openingTags.add(tag);
-            }
+            if(!tag.equals("<br>") && tag.charAt(1) != '/') stack.push(tag);
 
-            // If tag is closing tag = checking if it is equal with the opening tag that needs to be closed first
+            // If tag is closing tag - checking if it is equal with the opening tag that needs to be closed first
             if(!tag.equals("<br>") && tag.charAt(1) == '/') {
-                String closingTag = tag.substring(0, 1) + tag.substring(2);
+                String closingTag = tag.charAt(0) + tag.substring(2);
 
                 // If it is equal, the document continues to be valid and removing last opening tag
-                if(openingTags.get(openingTags.size() - 1).equals(closingTag)) {
-                    openingTags.remove(openingTags.size() - 1);
-                }
+                if(stack.getFirst().equals(closingTag)) stack.pop();
+
                 // If it is not equal, the document is invalid and there is no need to check remaining tags
                 else {
                     valid = false;
@@ -48,11 +43,11 @@ public class Q3 {
             }
         }
 
-        // Closing file reader and returning document's state
-        fileReader.close();
+        // Returning document's state
         return valid;
     }
 
+    // Main method to test sample files with code
     public static void main(String[] args) throws FileNotFoundException {
         String[] files = {"tags_valid.txt", "tags_invalid.txt"};
 
