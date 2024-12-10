c4ff3020699c13c90df6591283e762a99790cba9
Karina <152496419+yakovenkoka@users.noreply.github.com>
2024-12-10 00:28:36 +0000

    Q1 was done

--- src/main/java/org/example/Q1/Box.java
+++ src/main/java/org/example/Q1/Box.java
@@ -1,1 +1,42 @@
+package Q1;
 
+public class Box implements IMeasurableContainer{
+    private double width;
+    private double weight;
+    private double depth;
+    private double length;
+
+    public Box(double width, double weight, double depth, double length) {
+        this.width = width;
+        this.weight = weight;
+        this.depth = depth;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+    
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getDepth() {
+        return depth;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    @Override
+    public double weight() {
+        return weight;
+    }
+
+    @Override
+    public double rectangularVolume() {
+        return width * length * depth;
+    }
+
+}
---
+++ src/main/java/org/example/Q1/IMeasurableContainer.java
@@ -0,0 +1,6 @@
+package Q1;
+
+public interface IMeasurableContainer {
+    double weight();
+    double rectangularVolume();
+}
